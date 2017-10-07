8c2ed30873a03d46443890471aa08c576834aa81
mactkg <mactkg@gmail.com>
2017-10-07 17:55:24 +0900

    parallel

--- 1007.rb
+++ 1007.rb
@@ -1,5 +1,4 @@
-input = gets.chomp
-r = input.match(%r!(\d+),(\d+)(R|L|T|B)(\d+)/(\d+),(\d+)(R|L|T|B)(\d+)!)
+require 'parallel'
 
 def calc(x, y, d, h)
   diff_x, diff_y = 0, 0
@@ -16,24 +15,23 @@ def calc(x, y, d, h)
     else
       return []
   end
-  
+
   current_x = x
   current_y = y
   h.times do |n|
     # center point
-    result.push("#{current_x},#{current_y}")
+    result.push([current_x, current_y])
 
     # wing area
     n.times do |n|
       n += 1
       case d
         when 'R','L'
-          result.push("#{current_x},#{current_y+n}")
-          result.push("#{current_x},#{current_y-n}")
+          result.push([current_x, current_y+n])
+          result.push([current_x, current_y-n])
         when 'T','B'
-          result.push("#{current_x+n},#{current_y}")
-          result.push("#{current_x-n},#{current_y}")
-
+          result.push([current_x-n, current_y])
+          result.push([current_x+n, current_y])
       end
     end
 
@@ -45,7 +43,13 @@ def calc(x, y, d, h)
   result
 end
 
-A = calc(r[1].to_i, r[2].to_i, r[3], r[4].to_i)
-B = calc(r[5].to_i, r[6].to_i, r[7], r[8].to_i)
-C = A + B
-p C.size - C.uniq.size
+input = gets.chomp
+a, b = input.split('/')
+
+results = Parallel.map([a, b]) do |triangle|
+  parsed = triangle.match(%r!(\d+),(\d+)(R|L|T|B)(\d+)!)
+  calc(parsed[1].to_i, parsed[2].to_i, parsed[3], parsed[4].to_i)
+end
+c = results.flatten(1)
+p c.size - c.uniq.size
+
